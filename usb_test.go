3cd0d56cbbbddc86f170af238c63b1d8ee75a4d5
Martin Holst Swende <martin@swende.se>
2023-09-20 08:27:29 +0200

    update tests to be less verbose

--- usb_test.go
+++ usb_test.go
@@ -16,6 +16,7 @@
 package usb
 
 import (
+	"fmt"
 	"os"
 	"runtime"
 	"sync"
@@ -24,20 +25,30 @@ import (
 
 // Tests that HID enumeration can be called concurrently from multiple threads.
 func TestThreadedEnumerateHid(t *testing.T) {
-	var pend sync.WaitGroup
-	for i := 0; i < 8; i++ {
+	var (
+		threads         = 8
+		errs    []error = make([]error, threads)
+		pend    sync.WaitGroup
+	)
+	for i := 0; i < threads; i++ {
 		pend.Add(1)
 
 		go func(index int) {
 			defer pend.Done()
 			for j := 0; j < 512; j++ {
 				if _, err := EnumerateHid(uint16(index), 0); err != nil {
-					t.Errorf("thread %d, iter %d: failed to enumerate: %v", index, j, err)
+					errs[index] = fmt.Errorf("thread %d, iter %d: failed to enumerate-hid: %v", index, j, err)
+					break
 				}
 			}
 		}(i)
 	}
 	pend.Wait()
+	for _, err := range errs {
+		if err != nil {
+			t.Error(err)
+		}
+	}
 }
 
 // Tests that RAW enumeration can be called concurrently from multiple threads.
@@ -47,20 +58,30 @@ func TestThreadedEnumerateRaw(t *testing.T) {
 		t.Skip("Linux on Travis doesn't have usbfs, skipping test")
 	}
 	// Yay, we can actually test this
-	var pend sync.WaitGroup
-	for i := 0; i < 8; i++ {
+	var (
+		threads         = 8
+		errs    []error = make([]error, threads)
+		pend    sync.WaitGroup
+	)
+	for i := 0; i < threads; i++ {
 		pend.Add(1)
 
 		go func(index int) {
 			defer pend.Done()
 			for j := 0; j < 512; j++ {
 				if _, err := EnumerateRaw(uint16(index), 0); err != nil {
-					t.Errorf("thread %d, iter %d: failed to enumerate: %v", index, j, err)
+					errs[index] = fmt.Errorf("thread %d, iter %d: failed to enumerate-raw: %v", index, j, err)
+					break
 				}
 			}
 		}(i)
 	}
 	pend.Wait()
+	for _, err := range errs {
+		if err != nil {
+			t.Error(err)
+		}
+	}
 }
 
 // Tests that generic enumeration can be called concurrently from multiple threads.
@@ -69,18 +90,28 @@ func TestThreadedEnumerate(t *testing.T) {
 	if os.Getenv("TRAVIS") != "" && runtime.GOOS == "linux" {
 		t.Skip("Linux on Travis doesn't have usbfs, skipping test")
 	}
-	var pend sync.WaitGroup
-	for i := 0; i < 8; i++ {
+	var (
+		threads         = 8
+		errs    []error = make([]error, threads)
+		pend    sync.WaitGroup
+	)
+	for i := 0; i < threads; i++ {
 		pend.Add(1)
 
 		go func(index int) {
 			defer pend.Done()
 			for j := 0; j < 512; j++ {
 				if _, err := Enumerate(uint16(index), 0); err != nil {
-					t.Errorf("thread %d, iter %d: failed to enumerate: %v", index, j, err)
+					errs[index] = fmt.Errorf("thread %d, iter %d: failed to enumerate: %v", index, j, err)
+					break
 				}
 			}
 		}(i)
 	}
 	pend.Wait()
+	for _, err := range errs {
+		if err != nil {
+			t.Error(err)
+		}
+	}
 }
